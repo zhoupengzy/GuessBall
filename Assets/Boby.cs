9360d24b8d2aa6b6fe6c0faeab4a0f3aa967989c
zhoupeng <zhoupengzy@163.com>
2020-07-14 18:51:00 +0800

    杯子移动交换

---
+++ Assets/Boby.cs
@@ -0,0 +1,45 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boby : MonoBehaviour {
+
+    // 球
+    public Transform Ball;
+
+    // 杯子
+    public Transform Cup;
+
+    // 坐标
+    public Vector3 orgPos;
+
+    private bool isShowBall;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenCup()
+    {
+        Ball.gameObject.SetActive(isShowBall);
+
+        Sequence tween = DOTween.Sequence();
+        // 打开杯子
+        tween.Append(Cup.DOLocalMoveY(3, 2));
+
+    }
+
+    public void Reset()
+    {
+        Ball.gameObject.SetActive(true);
+        this.transform.localPosition = orgPos;
+        //Cup.localPosition = orgPos;
+    }
+}
---
+++ Assets/game.cs
@@ -0,0 +1,79 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour {
+
+    
+    // 主体
+    public Transform[] Boby;
+
+    public Vector3[] orgPos;
+
+    // Use this for initialization
+    void Start () {
+
+// 		for (int i = 0; i < Boby.Length; i++)
+//         {
+//             orgPos[i] = Boby[i].localPosition;
+//         }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickStart()
+    {
+        OnClickReset();
+        int[] indexs = GetRandom(2);
+
+
+        Vector3 midPos1 = new Vector3((orgPos[indexs[0]].x+orgPos[indexs[1]].x)/2, 0, 0.4f);
+        Vector3 midPos2 = new Vector3(midPos1.x, 0, -0.5f);
+        Vector3[] path1 = { orgPos[indexs[0]], midPos1, orgPos[indexs[1]]};
+        Vector3[] path2 = { orgPos[indexs[1]], midPos2, orgPos[indexs[0]]};
+
+        Sequence tween = DOTween.Sequence();
+        tween.Append(
+                Boby[indexs[0]].transform.DOLocalPath(path1, 0.6f, PathType.CatmullRom)
+                .SetOptions(false)
+                .SetEase(Ease.Linear)
+                );
+        tween.Join(
+                Boby[indexs[1]].transform.DOLocalPath(path2, 0.6f, PathType.CatmullRom)
+                .SetOptions(false)
+                .SetEase(Ease.Linear)
+                );
+
+    }
+
+
+    public void OnClickReset()
+    {
+        for (int i = 0; i < Boby.Length; i++)
+        {
+            Boby[i].GetComponent<Boby>().Reset();
+        }
+    }
+
+    private int[] GetRandom(int RmNum)
+    {
+        Hashtable hashtable = new Hashtable();
+        System.Random rm = new System.Random();
+        int[] RmValue = {0,1};
+        for (int i = 0; hashtable.Count < RmNum; i++)
+        {
+            int nValue = rm.Next(Boby.Length);
+            if (!hashtable.ContainsValue(nValue))
+            {
+                hashtable.Add(nValue, nValue);
+                RmValue[i] = nValue;
+            }
+        }
+        return RmValue;
+    }
+
+}
